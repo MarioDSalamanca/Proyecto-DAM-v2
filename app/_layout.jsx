04c284719986aa86d96a329840da0c4e006a3fda
MarioDSalamanca <dieguezmario07@gmail.com>
2024-12-16 10:35:17 +0100

    Prueba

---
+++ app/_layout.jsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { View, Text } from "react-native";
+
+export default function Layout() {
+    <View>
+        <Stack>
+            <Text>Holaaaaaaa</Text>
+        </Stack>
+    </View>
+}
